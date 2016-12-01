916a1caf063e025657216582f9c9d0d653312fb0
jackyhsuch <jackyhsu0102@gmail.com>
2016-12-01 17:11:46 +0800

    add order for when pulling tower data

--- app/controllers/towers_controller.rb
+++ app/controllers/towers_controller.rb
@@ -2,12 +2,12 @@ class TowersController < ApplicationController
     before_action :logged_in_user, only: [:index, :show]
 
 	def index
-		@towers = Tower.all
+		@towers = Tower.all.order(id: :asc)
 	end
 
 	def show
 		@tower = Tower.find(params[:id])
-		@topics = Topic.where(tower_id: params[:id])
+		@topics = Topic.where(tower_id: params[:id]).order(id: :asc)
 	end
 
 end
